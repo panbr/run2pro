d857dea85fc6f704231d9dd10c1e78ac1574fca0
panbr <115159694@qq.com>
2018-09-14 01:20:43 +0800

    add href

--- src/data/course.js
+++ src/data/course.js
@@ -4,62 +4,44 @@ export default {
     result: [{
         title: 'Level 1',
         list: [{
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '红旗飘飘',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/flag/',
             imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
             level: ['实战', '初级'],
             star: [1, 1, 1, 0, 0]
         }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '美国队长的盾牌',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/america_hero/',
             imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
             level: ['实战', '初级'],
             star: [1, 1, 1, 0, 0]
         }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '贪吃蛇游戏',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/snake/',
             imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
             level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
+            star: [1, 1, 1, 1, 0]
         }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '水墨时钟',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/clock/',
             imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
             level: ['实战', '初级'],
             star: [1, 1, 1, 0, 0]
         },  {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '粒子文字',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/atom_word/',
             imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
             level: ['实战', '初级'],
             star: [1, 1, 1, 0, 0]
         },  {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '散落的粒子',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/atom_down/',
             imgUrl: 'https://img3.mukewang.com/szimg/5b95cd0e0001e8c406000338.jpg',
             level: ['实战', '初级'],
             star: [1, 1, 1, 0, 0]
@@ -67,128 +49,92 @@ export default {
     }, {
         title: 'Level 2',
         list: [{
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '鼠标追踪',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/mouse_run/',
             imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
+            level: ['实战', '中级'],
             star: [1, 1, 1, 0, 0]
         }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '黑客帝国',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/hack/',
             imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
+            level: ['实战', '中级'],
+            star: [1, 1, 1, 1, 0]
+        }, {
+            name: '算数游戏',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/game_number/',
+            imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
+            level: ['实战', '中级'],
             star: [1, 1, 1, 0, 0]
         }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '俄罗斯方块游戏',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/fang/',
             imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            level: ['实战', '中级'],
+            star: [1, 1, 1, 1, 0]
+        },  {
+            name: '打飞机游戏',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/game_plane/',
             imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
+            level: ['实战', '中级'],
             star: [1, 1, 1, 0, 0]
         },  {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '文字绘图',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/word_draw/',
             imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        },  {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img4.mukewang.com/5afe7ffa00018fff06000338-240-135.jpg',
-            level: ['实战', '初级'],
+            level: ['实战', '中级'],
             star: [1, 1, 1, 0, 0]
         }]
     }, {
         title: 'Level 3',
         list: [{
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '你画我猜',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/draw/',
             imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
+            level: ['实战', '高级'],
+            star: [1, 1, 1, 1, 1]
+        }, {
+            name: '拯救小鱼',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/fish/',
+            imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
+            level: ['实战', '高级'],
             star: [1, 1, 1, 0, 0]
         }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '夜空中的烟花',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/firework/',
             imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
+            level: ['实战', '高级'],
+            star: [1, 1, 1, 1, 1]
         }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '吃豆豆游戏',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/game_dou/',
             imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
+            level: ['实战', '高级'],
+            star: [1, 1, 1, 1, 0]
         },  {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '环绕飞行',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/circle_fly/',
             imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
+            level: ['实战', '高级'],
+            star: [1, 1, 1, 1, 0]
         },  {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
+            name: '打砖块游戏',
+            label: ['js', 'canvas'],
+            href: 'http://run2code.com/projects/game_zhuan/',
             imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
-        }, {
-            name: '理解微服务核心思想',
-            label: ['java', '微服务'],
-            imgUrl: '//img2.mukewang.com/5b8ce427000121b406000338-240-135.jpg',
-            level: ['实战', '初级'],
-            star: [1, 1, 1, 0, 0]
+            level: ['实战', '高级'],
+            star: [1, 1, 1, 1, 0]
         }]
     }]
 }
